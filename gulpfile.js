672017f27f5bc0fe484a73e224cffc08114fd8ec
narcom <narcomacer@gmail.com>
2019-12-27 07:11:13 +0500

    start land2 07:11

--- gulpfile.js
+++ gulpfile.js
@@ -46,15 +46,15 @@ var browserSync = require('browser-sync').create();
 
 var config = {
     path: {
-        less: 'bs/src/less/*.less',
-        html: 'bs/public/index.html',
+        less: 'land2/src/less/*.less',
+        html: 'land2/public/index.html',
 
     },
     output: {
         cssName: 'bundle.min.css',
-        path: 'bs/public',
-        path_file: 'bs/public/index.html',
-        path_file_css: 'bs/public/bundle.min.css',
+        path: 'land2/public',
+        path_file: 'land2/public/index.html',
+        path_file_css: 'land2/public/bundle.min.css',
         newHtml: '/tmp/fz3temp-2'
     }
 }
